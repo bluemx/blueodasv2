353581b1e1f77ca25e0b47d07d526b9216944513
Albin Rodriguez <ealbinu@gmail.com>
2022-05-16 13:49:26 -0500

    upd

--- basicas/assejs/libs-dist.js
+++
@@ -1,5 +0,0 @@
-//import './libs/js/tailwind.js';
-//import './libs/js/mitt.js';
-//import './libs/js/howler.js';
-//import './libs/js/dom-to-image.js';
-//import 'https://cdnjs./cloudflare./com/ajax/libs/iconify/2.2.1/iconify.min.js';
